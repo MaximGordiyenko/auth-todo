4ad4a06b64fa0b0d4d4bb68d7c64787954c1cca3
Maxim <maxim.gordiyenko@gmail.com>
2019-10-07 20:40:00 +0300

    add fetch to register

--- backend/auth/AuthController.js
+++ backend/auth/AuthController.js
@@ -15,23 +15,43 @@ authRouter.use(bodyParser.json());
 authRouter.post(
   '/login',
   (req, res) => {
-      res.status(500).send('database is down')
-      // User.findOne({email: req.body.email}, function (err, user) {
-      //     if (err) return res.status(500).send('Error on the server.');
-      //     if (!user) return res.status(404).send('No user found.');
-      //
-      //     // .compareSync() method compare the password sent with the request to the password in the database
-      //     let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-      //     if (!passwordIsValid) return res.status(401).send({auth: false, token: null});
-      //
-      //     let token = jwt.sign({id: user._id}, config.secret, {
-      //         expiresIn: 86400 // expires in 24 hours
-      //     });
-      //
-      //     res.status(200).send({auth: true, token: token});
-      // });
+      console.log(req.body);
+      User.findOne({
+            name: req.body.name
+        },
+        (err, user) => {
+            if (err) return res.status(500).send('Error on the server.');
+            if (!user) return res.status(404).send('No user found.');
+
+            // .compareSync() method compare the password sent with the request to the password in the database
+            let passwordIsValid = bcrypt.compareSync(
+              req.body.password,
+              user.password
+            );
+            if (!passwordIsValid) return res.status(401).send({
+                auth: false,
+                token: null
+            });
+
+            let token = jwt.sign({
+                  id: user._id
+              },
+              config.secret, {
+                  expiresIn: 86400 // expires in 24 hours
+              });
+
+            res.status(200).send({
+                auth: true,
+                token: token,
+                user:  {
+                   id: user._id,
+                    username: user.name
+                }
+            });
+        });
 
   });
+
 // use get on browser form load
 authRouter.get(
   '/register',
@@ -41,7 +61,7 @@ authRouter.get(
       res.status(200).sendFile('/Users/maksim/Desktop/auth-todo/frontend/register.html');
   }
 );
-
+//create user in db and return auth & token to web
 authRouter.post(
   '/register',
   (req, res) => {
@@ -55,23 +75,67 @@ authRouter.post(
             email: req.body.email,
             password: hashedPassword
         },
-        (err, user) => {
-            if (err) return res.status(500).send("There was a problem registering the user.");
+        (err, data) => {
+            if (err) return res.status(500).send("There was a problem registering the data.");
             // create a token
             let token = jwt.sign(
-              {id: user._id},
+              {id: data._id},
               config.secret, {
-                expiresIn: 44200
-            });
+                  expiresIn: 44200
+              });
             res.status(200).send({
-                auth: true,
-                token: token
+                token: token,
+                name: data.name,
+                email: data.email
             });
         })
   });
+//return all users from db
+authRouter.get(
+  '/users',
+  (req, res) => {
+      User.find(
+        {},
+        (err, users) => {
+            if (err) return res.status(500).send("There was a problem finding the users.");
+            res.status(200).send(users);
+        });
+  });
+
+//return one user
+authRouter.get(
+  '/users/:id',
+  (req, res) => {
+      User.findById(
+        req.params.id,
+        (err, user) => {
+            if (err) return res
+              .status(500)
+              .send("There was a problem finding the user.");
+            if (!user) return res
+              .status(404).send("No user found.");
+            res.status(200).send(user);
+        });
+  });
+
+//remove one user
+authRouter.delete(
+  '/users/:id',
+  (req, res) => {
+      User.findByIdAndRemove(
+        req.params.id,
+        (err, user) => {
+            if (err) return res
+              .status(500)
+              .send("There was a problem deleting the user.");
+            res
+              .status(200)
+              .send("User: " + user.name + " was deleted.");
+        });
+  });
 
 authRouter.get(
-  '/logout',
+  'users/logout/:id',
   (req, res) => {
       res.status(200)
         .send({
@@ -80,18 +144,21 @@ authRouter.get(
         });
   });
 
-authRouter.get(
-  '/me',
+authRouter.put(
+  'users/:id',
   VerifyToken,
-  (req, res, next) => {
-      User.findById(
-        req.userId,
-        {password: 0},
+  (req, res) => {
+      User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true},
         (err, user) => {
-            if (err) return res.status(500).send("There was a problem finding the user.");
-            if (!user) return res.status(404).send("No user found.");
-
-            res.status(200).send(user);
+            if (err) return res
+              .status(500)
+              .send("There was a problem updating the user.");
+            res
+              .status(200)
+              .send(user);
         });
   });
 
