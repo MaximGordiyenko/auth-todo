afb9789bf76999fbb7a09c2b4c820a44a4549ccb
Maxim <maxim.gordiyenko@gmail.com>
2019-10-23 22:59:07 +0300

    implemented todo add router

--- backend/todo.route.js
+++ backend/todo.route.js
@@ -5,18 +5,20 @@ let Todo = require('./todo.model');
 let jwt = require('jsonwebtoken');
 let config = require('../backend/config');
 
-//return user id on success or empty string
-function checkTokenAccess(token) {
-    let userID = '';
+//call_on_id(userID) is called when token is okay and provides valid userID,
+//call_on_id(null) otherwise.
+function on_userid_action(token, call_on_id) {
     jwt.verify(
       token,
       config.secret,
       (err, decoded) => {
           if (err)
-              return null;
-          userID = decoded.id;
+              //token invalid
+              call_on_id(null);
+          else
+              //token is valid - callback with userID string.
+              call_on_id(decoded.id)
       });
-    return userID;
 }
 
 todoRoutes.get(
@@ -31,77 +33,83 @@ todoRoutes.get(
       // verify auth credentials
       const token = req.headers.authorization;
       console.log("the token is ", token);
-      let userID = checkTokenAccess(token);
-      if ('' === userID) {
-          res.status(401).json({err: 'user id not found'});
-      } else {
-          Todo.find(
-            {user_id: userID},
-            (err, todos) => {
-                if (err) {
-                    res.status(404).json({err: 'TODO not found'});
-                    console.log(err);
-                } else if (null === todos || todos.length === 0) {
-                    console.log('Todos list is empty');
-                    res.send(
-                      `<html>
+      on_userid_action(token, function (userID) {
+          if (null === userID) {
+              res.status(401).json({err: 'user id not found'});
+              return;
+          }
+          Todo.find({user_id: userID},
+                (err, todos) => {
+                    if (err) {
+                        res.status(404).json({err: 'TODO not found'});
+                        console.log(err);
+                        return;
+                    }
+                    if (null === todos || todos.length === 0) {
+                        console.log('Todos list is empty');
+                        res.send(
+                          `<html>
 
 <body>
 <h3>Todos list empty</h3>
 <a href="/add.html">[+] add todo</a>
 </body>
                       </html>`
-                    );
-                } else {
+                        );
+                        return;
+                    }
                     console.log('Todos list is not empty');
                     let todoListHtml = "<html> <body>";
                     todoListHtml += todos.toString();
                     todos.forEach(function (value) {
-                       todoListHtml += "<p>" + value.toString() + "</p>";
+                        todoListHtml += "<p>" + value.toString() + "</p>";
                     });
                     todoListHtml += "</html> </body>";
                     res.send(todoListHtml);
-                }
-            });
-      }
+                });
+        });
   });
 
 todoRoutes.post(
   '/add',
   (req, res) => {
       console.log('working out /add');
+      const token = req.headers.authorization;
       // check for basic auth header
-      if (null === req.headers.authorization) {
+      if (null === token || '' === token) {
           console.log('null Authorization object');
           return res.status(401).json({err: 'Missing Authorization Header'});
       }
       // verify auth credentials
-      const token = req.headers.authorization;
       console.log("the token is ", token);
-      // got no userid ??
-      let userID = checkTokenAccess(token);
-      console.log(userID); //check this
-      if (null == userID || '' === userID) {
-          console.log('/todos/add token got no user_id');
-          res.status(401).json({err: 'user id not found'});
-          return;
-      }
-      console.log('/todos/add userid = ' + userID);
-      const body_json = req.body;
-      res.json('Todos updated!');
-      let object = Todo.create({ "text" : body_json.todo,
-        "user_id" : userID,
-        "completed" : false,
-        "create_data" : body_json.date});
-      console.log("todo created " + object.toString());
-      res.status(200);
+      on_userid_action(token, function (userID) {
+          console.log("id is " + userID); //check this
+          if (null == userID || '' === userID) {
+              console.log('/todos/add token got no user_id');
+              res.status(401).json({err: 'user id not found'});
+              return;
+          }
+          console.log('/todos/add userid = ' + userID);
+          const body_json = req.body;
+          res.json('Todos updated!');
+          let object = Todo.create({
+              "text": body_json.todo,
+              "user_id": userID,
+              "completed": false,
+              "create_data": body_json.date
+          });
+          object.then(function (todo_item) {
+              console.log("todo created " + todo_item);
+          });
+          res.status(200);
+      });
   });
 /*
  todoRoutes.delete(
  '/:id',
  (req, res) => {
  let token = req.params.id;
- let userID = checkTokenAccess(token);
+ let userID = on_userid_action(token);
  if ('' === userID) {
  res.status(404);
  return;
